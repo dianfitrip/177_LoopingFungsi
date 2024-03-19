cddb79d5fe74bdef1b678acbf7b48dfde5ef8f22
dianfitrip <dianfitri683@gmail.com>
2024-03-19 10:25:09 +0700

    perulanagn FOR

--- Perulangan_fungsi_procedure_19Maret/Perulangan_fungsi_procedure_19Maret.cpp
+++ Perulangan_fungsi_procedure_19Maret/Perulangan_fungsi_procedure_19Maret.cpp
@@ -7,6 +7,17 @@ int main() {
 	int arr[5];
 
 	for (i = 60; i > 10; i -= 10) {
-
+		cout << i << "Selamat Pagi Dunia" << endl;
 	}
+
+	cout << "Nilai i Terakhir : " << i << endl;
+
+	for (i = 0; i < 5; i++) {
+		cout << "Masukkan Nilai index ke-" << i << " :";
+		cin >> arr[i];
+	}
+	for (i = 0; i < 5; i++) {
+		cout << "Data Array ke-" << i << " :" << arr[i] << endl;
+	}
+	return 0;
 }
